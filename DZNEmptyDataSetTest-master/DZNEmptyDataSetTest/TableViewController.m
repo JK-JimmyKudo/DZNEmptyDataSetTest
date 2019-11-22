8704ea54076fe780b44e0656987f5e51d568f546
pengwei <pengwei6@asiainfo.com>
2019-11-22 14:31:55 +0800

    添加注释

--- DZNEmptyDataSetTest-master/DZNEmptyDataSetTest/TableViewController.m
+++ DZNEmptyDataSetTest-master/DZNEmptyDataSetTest/TableViewController.m
@@ -50,8 +50,13 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-      NSLog(@"didSelectRowAtIndexPath");
+    
+    NSLog(@"didSelectRowAtIndexPath");
+    
+    
+    
 }
 
 
